c4e2fc2fe522817a714190504a5bab724f47c5ca
Peter <97176773+PeterZhang26@users.noreply.github.com>
2023-08-23 15:28:54 +0200

    Updated main loop parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils
 
@@ -18,13 +19,19 @@ top_frame = Frame(
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
-    root, bg="blue", width=utils.width_prct(25), height=utils.height_prct(75)
+    root, bg="black", width=utils.width_prct(25), height=utils.height_prct(75)
 )
 left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(
-    root, bg="green", width=utils.width_prct(75), height=utils.height_prct(75)
+    root, bg="black", width=utils.width_prct(75), height=utils.height_prct(75)
 )
 center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(column=x, row=y)
 # Run the window
 root.mainloop()
